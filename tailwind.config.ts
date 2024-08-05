dc844684783755d0dcc27162c7da39c181080054
Sojin Lee <sojinlee0z0@gmail.com>
2024-08-05 22:52:41 +0900

    Design: 폰트, 색상 지정
    폰트
    자주 사용할 타이포
    색상 지정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,43 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    extend: {},
+    colors: {
+      soft: '#E6F0FA',
+
+      primary: '#1E90FF',
+      primarDark: '##0056b3',
+      primaryDarker: '##003d80',
+      primaryLight: '#63B8FF',
+      primaryLighter: '#B0E0E6',
+
+      neutral: '#333333',
+      neutralLight: '#666666',
+    },
+    fontSize: {
+      'display-l': `clamp(3.0rem, 2.5vw, 5.0rem)`,
+      'display-m': `clamp(2.5rem, 2.0vw, 4.0rem)`,
+      'display-s': `clamp(2.0rem, 1.75vw, 3.0rem)`,
+      'headline-l': `clamp(2.0rem, 1.5vw, 3.0rem)`,
+      'headline-m': `clamp(1.75rem, 1.25vw, 2.5rem)`,
+      'headline-s': `clamp(1.5rem, 1.0vw, 2.0rem)`,
+      'title-l': `clamp(1.5rem, 1.0vw, 2.0rem)`,
+      'title-m': `clamp(1.25rem, 0.9vw, 1.75rem)`,
+      'title-s': `clamp(1.0rem, 0.8vw, 1.5rem)`,
+      'body-l': `clamp(1.0rem, 0.75vw, 1.25rem)`,
+      'body-m': `clamp(0.875rem, 0.65vw, 1.125rem)`,
+      'body-s': `clamp(0.75rem, 0.6vw, 1.0rem)`,
+    },
+    fontFamily: {
+      sans: ['Pretendard', 'sans-serif'],
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
