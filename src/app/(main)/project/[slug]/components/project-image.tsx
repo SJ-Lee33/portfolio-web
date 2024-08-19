34272321ebd04e561320e88f4670b68b4477d334
Sojin Lee <sojinlee0z0@gmail.com>
2024-08-19 18:19:29 +0900

    feat: 프로젝트상세 block 부분 링크 추가

--- src/app/(main)/project/[slug]/components/project-image.tsx
+++ src/app/(main)/project/[slug]/components/project-image.tsx
@@ -4,10 +4,12 @@ export default function ProjectImage({ asset }: { asset: { _ref: string } }) {
   const imageUrl = urlFor({ _ref: asset._ref }).url()
 
   return (
-    <img
-      src={imageUrl}
-      alt="본문 이미지"
-      className="w-[400px] h-auto object-contain mx-4 md:mx-10 my-3 shadow-md shadow-neutralLight/30"
-    />
+    <div className="p-4 md:px-10">
+      <img
+        src={imageUrl}
+        alt="본문 이미지"
+        className="w-[400px] h-auto object-contain shadow-md shadow-neutralLight/30"
+      />
+    </div>
   )
 }
--- src/app/(main)/project/[slug]/components/project-plaintext.tsx
+++ src/app/(main)/project/[slug]/components/project-plaintext.tsx
@@ -1,3 +1,35 @@
-export default function ProjectPlanetext({ children }: { children: any }) {
-  return <p className="w-full px-4 py-2 md:px-10 text-body-m">{children}</p>
+import Link from 'next/link'
+import React from 'react'
+
+export default function ProjectPlanetext({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  // 노멀텍스트 조건부 스타일링을 위한 함수
+  const renderChild = (child: React.ReactNode): React.ReactNode => {
+    if (React.isValidElement(child) && child.props?.markType === 'link') {
+      // 링크이면
+      return (
+        <Link
+          key={child.key}
+          className="text-primaryDark underline"
+          href={child.props.value.href}
+          target="_blank"
+        >
+          {child.props.children}
+        </Link>
+      )
+    }
+
+    return child
+  }
+
+  return (
+    <p className="w-full px-4 my-3 md:px-10 text-body-l">
+      {Array.isArray(children)
+        ? children.map(renderChild)
+        : renderChild(children)}
+    </p>
+  )
 }
