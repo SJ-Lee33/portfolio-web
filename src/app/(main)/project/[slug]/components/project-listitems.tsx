05e6deaac31999b0757bc1b1c8694b290b0e9737
Sojin Lee <sojinlee0z0@gmail.com>
2024-08-19 18:19:47 +0900

    design: 프로젝트상세 block 부분 ul 번호매기게
    disc → decimal

--- src/app/(main)/project/[slug]/components/project-listitems.tsx
+++ src/app/(main)/project/[slug]/components/project-listitems.tsx
@@ -1,8 +1,8 @@
 export default function ProjectListitems({ children }: { children: any[] }) {
   return (
     <ul
-      className="mx-8 my-2 md:mx-16 text-body-m"
-      style={{ listStyleType: 'disc' }}
+      className="mx-8 my-2 md:mx-16 text-body-l font-medium"
+      style={{ listStyleType: 'decimal' }}
     >
       {children}
     </ul>
