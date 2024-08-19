2c3378027e25412dcf433e4810e949fab20878c9
Sojin Lee <sojinlee0z0@gmail.com>
2024-08-19 14:13:35 +0900

    fix: gethandler 수정

--- src/app/api/profile/handlers/get-handlers.ts
+++ src/app/api/profile/handlers/get-handlers.ts
@@ -1,10 +1,14 @@
 import { client } from '@/lib/sanity'
 
 // 프로필 정보 불러오기
-export async function getHandler() {
+export async function getHandler(request: Request) {
   try {
-    const data = await getData()
+    const url = new URL(request.url)
 
+    const page = url.searchParams.get('page') ?? '1'
+    const limit = url.searchParams.get('limit') ?? '50'
+
+    const data = await getData({ page: parseInt(page), limit: parseInt(limit) })
     return new Response(
       JSON.stringify({
         data,
@@ -22,15 +26,14 @@ export async function getHandler() {
   }
 }
 
-const getData = async () => {
-  const query = `*[_type == "history"  && !(_id in path('drafts.**'))] | order(year desc) [
-  ]
-    {
-      'id':_id,
-      year,
+const getData = async ({ page, limit }: { page: number; limit: number }) => {
+  const query = `*[_type == "history" && !(_id in path('drafts.**')) ] | order(year desc) [
+    ${limit * (page - 1)}...${limit * page}
+    ]  {
+    'id': _id,
+    year,
       content,
-    }
-    `
+ }`
 
   const data = await client.fetch(query)
   const history = data.map((history: any) => {
--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -2,5 +2,5 @@ import 'server-only'
 import { getHandler } from './handlers/get-handlers'
 
 export async function GET(request: Request) {
-  return getHandler()
+  return getHandler(request)
 }
