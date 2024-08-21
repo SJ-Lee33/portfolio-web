470f0963ef808e196c272e88ac85c9d3f918c79f
Sojin Lee <sojinlee0z0@gmail.com>
2024-08-21 20:55:53 +0900

    design: 네비게이션바 애니메이션 추가

--- src/app/(main)/(home)/home-page.tsx
+++ src/app/(main)/(home)/home-page.tsx
@@ -7,13 +7,42 @@ import ThirdScreen from './components/third-screen'
 import LastScreen from './components/last-screen'
 import { LoadingSpinner } from '@/components/loading-spinner'
 import { Scroll } from './components/scroll'
+import NavBar from '@/components/nav-bar/nav-bar'
+import { useEffect, useState } from 'react'
 
 const HomePage: React.FC = () => {
   const profileRef = useRef<HTMLDivElement>(null)
   const projectRef = useRef<HTMLDivElement>(null)
+  const [headerDesign, setHeaderDesign] = useState('')
+  const [shownLogo, setShownLogo] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const position = window.scrollY
+
+      if (position >= 660) {
+        // 스크롤 진행후
+        setHeaderDesign('bg-white text-neutral shadow-md shadow-neutral/5')
+        setShownLogo(true)
+      } else {
+        // 맨위일때
+        setHeaderDesign('bg-opacity-0 text-white')
+        setShownLogo(false)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
 
   return (
     <div>
+      <header className="w-full fixed top-0 z-50">
+        <NavBar shownLogo={shownLogo} headerDesign={headerDesign} />
+      </header>
       <FirstScreen />
       <div ref={profileRef}>
         <SecondScreen />
