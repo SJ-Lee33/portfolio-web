3ac39fac2dcf85a6b61945e7010adf5f19a98c68
sj.lee <sojinlee0z0@gmail.com>
2025-04-07 07:47:21 +0900

    fix: 트러블슈팅 이미지 번역
    이미지 구조를 url로 번역하도록 수정

--- src/app/(main)/project/[slug]/components/project-image.tsx
+++ src/app/(main)/project/[slug]/components/project-image.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
 
-// ✅ 클라이언트 컴포넌트에서 안전하게 사용
 export default function ProjectImage({ url }: { url: string }) {
   return (
     <div className="p-4 md:px-10 mt-2 mb-[30px]">
