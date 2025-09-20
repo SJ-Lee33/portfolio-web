08d44184df9ecd8388d16adfd2c269eb9a750416
Sojin Lee <sojinlee0z0@gmail.com>
2025-09-20 18:53:15 +0900

    fix: 환경변수 수정

--- sanity.config.ts
+++ sanity.config.ts
@@ -23,8 +23,8 @@ const PublishWithSerial = createPublishWithSerialAction({
 export default defineConfig({
   basePath: '/studio',
   title: 'portfolio',
-  projectId: process.env.SANITY_STUDIO_PROJECT_ID!,
-  dataset: process.env.SANITY_DATASET!,
+  projectId: projectId,
+  dataset: dataset,
   // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema: {
     types: schema,
@@ -33,7 +33,7 @@ export default defineConfig({
     structureTool({ structure }),
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({ defaultApiVersion: process.env.SANITY_API_VERSION! }),
+    visionTool({ defaultApiVersion: apiVersion }),
     codeInput(),
   ],
   document: {
